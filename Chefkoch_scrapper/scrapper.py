9410040e173101e01f8f92a32f8ab065b15a0753
nataliasverchkova <nsv@hellofresh.com>
2017-07-07 12:52:33 +0200

    scrapper with categories

--- Chefkoch_scrapper/scrapper.py
+++ Chefkoch_scrapper/scrapper.py
@@ -22,7 +22,7 @@ def get_total_pages(html):
 
     return int(total_pages)
 
-def write_csv(data):
+def write_to_recipes(data):
     with open('chefkoch.csv', 'a', newline='') as f:
         writer = csv.writer(f)
 
@@ -52,11 +52,14 @@ def get_front_page(html):
     lis = soup.find_all('li', class_="search-list-item")
 
     for index, li in enumerate(lis):
+
+        # get recipe ID
         try:
             id_r = li.get('id')
         except:
             id_r = ''
 
+        # save picture and if recipe has one
         try: 
             if li.find('picture') is not None:
                 img_link = get_picture_link(li)
@@ -68,48 +71,57 @@ def get_front_page(html):
         except:
             has_pic = ''
 
+        # link to the full recipe info
         try:
             koch_url = 'http://www.chefkoch.de'
             link = koch_url + li.find('a').get('href')
         except:
             link = ''
 
+        # name of the recipe
         try:
-            name = li.find('div', class_='search-list-item-title').text.encode('utf-8').strip()
+            name = li.find('div', class_='search-list-item-title').text.encode('latin-1').strip()
         except:
             name = ''
 
+        # average number of stars for recipe
         try:
             stars = li.find('span', class_='search-list-item-uservotes-stars').get('title')
         except:
             stars = ''
 
+        # number of stars shown near recipe
         try:
             stars_shown = li.find('span', class_='search-list-item-uservotes-stars').get('class')[1]
         except:
             stars_shown = ''
 
+        # number of votes
         try:
             votes = li.find('span', class_='search-list-item-uservotes-count').text.strip()
         except:
             votes = ''
 
+        # difficulty of the recipe
         try:
             difficulty = li.find('span', class_='search-list-item-difficulty').text.strip()
 
         except:
             difficulty = ''
 
+        # time to prepare recipe
         try:
             preptime = li.find('span', class_='search-list-item-preptime').text.strip()
         except:
             preptime = ''
 
+        # date of reipe published
         try:
             date = li.find('span', class_='search-list-item-activationdate').text.strip()
         except:
             date = ''
 
+        # write to dictionary
         data = {'recipe_id' : id_r,
                 'recipe_name' : name,
                 'average_rating': stars,
@@ -120,8 +132,9 @@ def get_front_page(html):
                 'has_picture' : has_pic,
                 'date' : date,
                 'link' : link}
-
-        write_csv(data)
+        
+        # append to the file 
+        write_to_recipes(data)
 
 def please_scrap(url):
     print(url)
@@ -129,10 +142,14 @@ def please_scrap(url):
     get_front_page(html) 
 
 
+
+
+
+
 def main():
 
     web_url = 'http://www.chefkoch.de'
-    os.chdir(r'C:\\Users\Natalia\Documents\Thesis\Chefkoch_scrapper')
+    os.chdir(r'C:\\Users\Natalia\Documents\Thesis\scrap_try')
 
     start = datetime.now()
 
@@ -151,26 +168,27 @@ def main():
         
     print(urls_categories)
 
-    start_url = 'http://www.chefkoch.de/rs/s0o3' + urls_categories[0]
-    total_pages = get_total_pages(get_html(start_url))
-    print(start_url, total_pages)
+    start_url = 'http://www.chefkoch.de/rs/s'
 
-    first_part = 'http://www.chefkoch.de/rs/s'
-    category_part = urls_categories[0]
-    url_list = []
-    for i in range(0, total_pages + 1):
-        
-        url_to_scrap = first_part + str(i * 30) + 'o3' + category_part
-        url_list.append(url_to_scrap)
-        # print(url_to_scrap)
-        # get_front_page(get_html(url_to_scrap))
+    for category in urls_categories:
 
-    print(url_list)
+        category_url = start_url + '0o3' + category
+        total_pages = get_total_pages(get_html(category_url))
+        # print(start_url, total_pages)
 
-    # please_scrap('http://www.chefkoch.de/rs/s2970g47/Suessspeisen-Backen-Rezepte.html')
+        url_list = []
+        for i in range(0, total_pages + 1):
+        # for i in range(0, 10):
+            
+            url_to_scrap = start_url + str(i * 30) + 'o3' + category
+            url_list.append(url_to_scrap)
+            # print(url_to_scrap)
+            # get_front_page(get_html(url_to_scrap))
 
-    with Pool(20) as p:
-        p.map(please_scrap, url_list)
+        # print(url_list)
+
+        with Pool(20) as p:
+            p.map(please_scrap, url_list)
 
     end = datetime.now()
     total = end - start
