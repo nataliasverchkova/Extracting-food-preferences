63060f40404be4b52df7ef3c86159e06a97157de
nataliasverchkova <nsv@hellofresh.com>
2017-07-10 15:13:20 +0200

    no categories

--- Chefkoch_scrapper/scrapper.py
+++ Chefkoch_scrapper/scrapper.py
@@ -148,47 +148,28 @@ def please_scrap(url):
 
 def main():
 
-    web_url = 'http://www.chefkoch.de'
-    os.chdir(r'C:\\Users\Natalia\Documents\Thesis\scrap_try')
-
+    # http://www.chefkoch.de/rs/s30o3/Rezepte.html
+    
     start = datetime.now()
 
-    # Get necessary parts of URLs for all categories of recipes
-    soup = BeautifulSoup(get_html('http://www.chefkoch.de/rezepte/kategorien/'), 'lxml')
-    categories = soup.find_all('h2', class_="category-level-1")
-    urls_categories = []
-    for index, category in enumerate(categories):
-        
-        href = category.find('a').get('href')
-        category_number = href.split('/')[2].split('g')[1]
-        category_name   = href.split('/')[-1]
-        
-        link_part = 'g' + category_number + '/' + category_name
-        urls_categories.append(link_part)
-        
-    print(urls_categories)
-
     start_url = 'http://www.chefkoch.de/rs/s'
+    category  = '/Rezepte.html'
 
-    for category in urls_categories:
+    # for category in urls_categories:
 
-        category_url = start_url + '0o3' + category
-        total_pages = get_total_pages(get_html(category_url))
-        # print(start_url, total_pages)
+    category_url = start_url + '0o3' + category
+    total_pages = get_total_pages(get_html(category_url))
+    # print(start_url, total_pages)
 
-        url_list = []
-        for i in range(0, total_pages + 1):
-        # for i in range(0, 10):
-            
-            url_to_scrap = start_url + str(i * 30) + 'o3' + category
-            url_list.append(url_to_scrap)
-            # print(url_to_scrap)
-            # get_front_page(get_html(url_to_scrap))
+    url_list = []
+    for i in range(0, total_pages + 1):
+    # for i in range(0, 100):
+        
+        url_to_scrap = start_url + str(i * 30) + 'o3' + category
+        url_list.append(url_to_scrap)
 
-        # print(url_list)
-
-        with Pool(20) as p:
-            p.map(please_scrap, url_list)
+    with Pool(15) as p:
+        p.map(please_scrap, url_list)
 
     end = datetime.now()
     total = end - start
